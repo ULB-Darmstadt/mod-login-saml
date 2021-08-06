cc6cd7b102cccbc33f7fa585ad2b76cbfc842c78
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2021-08-06 13:26:55 +0100

    Add request level caching of headers.

--- src/main/java/org/folio/util/OkapiHelper.java
+++ src/main/java/org/folio/util/OkapiHelper.java
@@ -13,18 +13,27 @@ import java.util.Map;
 public class OkapiHelper {
 
 
+  private static final String CACHE_KEY = "OKAPI_HEADERS";
+  
   /**
    * Extract Okapi specific headers from current routing context
    */
-  public static OkapiHeaders okapiHeaders(RoutingContext routingContext) {
+  public static OkapiHeaders okapiHeaders( RoutingContext routingContext ) {
 
-    OkapiHeaders headers = new OkapiHeaders();
+    // Grab from request cache.
+    OkapiHeaders headers = routingContext.get(CACHE_KEY);
+    if (headers != null) return headers;
+    
+    // Not present yet, lets create it.
+    headers = new OkapiHeaders();
 
     headers.setUrl(routingContext.request().getHeader(OkapiHeaders.OKAPI_URL_HEADER));
     headers.setTenant(routingContext.request().getHeader(OkapiHeaders.OKAPI_TENANT_HEADER));
     headers.setToken(routingContext.request().getHeader(OkapiHeaders.OKAPI_TOKEN_HEADER));
     headers.setPermissions(routingContext.request().getHeader(OkapiHeaders.OKAPI_PERMISSIONS_HEADER));
-
+    
+    // Stash in the request safe context.
+    routingContext.put(CACHE_KEY, headers);
     return headers;
 
   }
