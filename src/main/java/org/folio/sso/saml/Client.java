b41342cf5640af8f3d1230558960788712eb6bdb
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2021-08-23 17:35:32 +0100

    feat: DiscoResponse entry in metadata.

--- src/main/java/org/folio/sso/saml/Client.java
+++ src/main/java/org/folio/sso/saml/Client.java
@@ -10,9 +10,14 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.config.JsonReponseSaml2RedirectActionBuilder;
 import org.folio.rest.tools.client.test.HttpClientMock2;
 import org.folio.sso.saml.Constants.Config;
+import org.folio.sso.saml.metadata.ExtendedSAML2ServiceProviderMetadataResolver;
 import org.folio.util.OkapiHelper;
 import org.folio.util.model.OkapiHeaders;
 import org.opensaml.saml.common.xml.SAMLConstants;
@@ -31,10 +36,6 @@ import org.pac4j.saml.state.SAML2StateGenerator;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.CompositeFuture;
@@ -46,64 +47,46 @@ import io.vertx.ext.web.RoutingContext;
 
 public class Client extends SAML2Client {
   
+  private static class MockClient extends Client {
+
+    private static final Logger log = LogManager.getLogger(MockClient.class);
+    public static final String SAML_USER_ID = "saml-user-id";
+
+    public MockClient(final SAML2Configuration cfg) {
+      super(cfg);
+      log.info("SAML2 Client MOCK mode");
+    }
+
+    @Override
+    protected Optional<SAML2Credentials> retrieveCredentials(WebContext context) {
+      log.info("Mocking Client retrieveCredentials...");
+
+      assert(context.getRequestParameter("SAMLResponse").isPresent());
+
+      NameID nameId = new NameIDBuilder().buildObject();
+      String issuerId = this.getClass().getName();
+      List<Attribute> samlAttributes = new ArrayList<>();
+      Conditions conditions = new ConditionsBuilder().buildObject();
+      List<String> authnContexts = new ArrayList<>();
+      SAML2Credentials cred = new SAML2Credentials(nameId, issuerId, samlAttributes, conditions, "1", authnContexts);
+
+      CommonProfile userProfile = new CommonProfile();
+      userProfile.addAttribute("UserID", Arrays.asList(SAML_USER_ID));
+      cred.setUserProfile(userProfile);
+      return Optional.of(cred);
+    }
+
+  }
+  
   private static final String CACHE_KEY = "SAML_CLIENT";
   private static final Logger log = LogManager.getLogger(Client.class);
-  private static final Map<String, Future<Client>> tenantCache = new ConcurrentHashMap<String, Future<Client>>();
 
-  public static void forceReinit(final String tenant) {
-    // Clear the cache for the single tenant...
-    tenantCache.remove(tenant);
+  private static final Map<String, Future<Client>> tenantCache = new ConcurrentHashMap<String, Future<Client>>();
+  
+  private static String buildCallbackUrl(String okapiUrl, String tenantId) {
+    return okapiUrl + "/_/invoke/tenant/" + CommonHelper.urlEncode(tenantId) + Config.CALLBACK_ENDPOINT;
   }
   
-  public static void forceReinit() {
-    // Clear the caches...
-    tenantCache.clear();
-  }
-  
-  private Client() {}
-  
-  private Client( SAML2Configuration cfg ) {
-    super(cfg);
-  }
-  
-  public static Future<Client> get ( final RoutingContext routingContext, final boolean generateMissingKeyStore, final boolean reinitialize ) {
-       
-    // Override this value if we are testing.
-    
-    // Get the okapi tenant header.
-    final String tenant = OkapiHelper.okapiHeaders(routingContext).getTenant();
-    
-    Future<Client> future;
-    if (!reinitialize) {
-      future = routingContext.get(CACHE_KEY);
-      if (future != null) {
-        log.debug("Returning client from request cache");
-        return future;
-      }
-      
-      // Not in the request. Check if we already have 1 in the tenant cache.
-      future = tenantCache.get(tenant);
-      if (future != null) {
-        if (future.succeeded()) {
-          // clear cache to allow cleanup.
-          log.debug("Returning client from tenant cache");
-          return future;
-        }
-        
-        // Cleanup.
-        tenantCache.remove(tenant);
-      }
-    }
-    
-    // Else create and cache in the request, and the tenant cache.
-    log.debug("Creating new client");
-    future = createClient( routingContext, generateMissingKeyStore );
-    
-    routingContext.put( CACHE_KEY, future );
-    tenantCache.put( tenant, future );
-    return future;
-  }
-    
   private static Future<Client> createClient(final RoutingContext routingContext, final boolean generateMissingKeyStore) {
     OkapiHeaders okapiHeaders = OkapiHelper.okapiHeaders(routingContext);
     final String tenantId = okapiHeaders.getTenant();
@@ -190,7 +173,97 @@ public class Client extends SAML2Client {
       return clientInstantiationFuture.future();
     });
   }
+  
+  public static void forceReinit() {
+    // Clear the caches...
+    tenantCache.clear();
+  }
+  
+  public static void forceReinit(final String tenant) {
+    // Clear the cache for the single tenant...
+    tenantCache.remove(tenant);
+  }
+  
+  public static Future<Client> get ( final RoutingContext routingContext, final boolean generateMissingKeyStore, final boolean reinitialize ) {
+       
+    // Override this value if we are testing.
+    
+    // Get the okapi tenant header.
+    final String tenant = OkapiHelper.okapiHeaders(routingContext).getTenant();
+    
+    Future<Client> future;
+    if (!reinitialize) {
+      future = routingContext.get(CACHE_KEY);
+      if (future != null) {
+        log.debug("Returning client from request cache");
+        return future;
+      }
+      
+      // Not in the request. Check if we already have 1 in the tenant cache.
+      future = tenantCache.get(tenant);
+      if (future != null) {
+        if (future.succeeded()) {
+          // clear cache to allow cleanup.
+          log.debug("Returning client from tenant cache");
+          return future;
+        }
+        
+        // Cleanup.
+        tenantCache.remove(tenant);
+      }
+    }
+    
+    // Else create and cache in the request, and the tenant cache.
+    log.debug("Creating new client");
+    future = createClient( routingContext, generateMissingKeyStore );
+    
+    routingContext.put( CACHE_KEY, future );
+    tenantCache.put( tenant, future );
+    return future;
+  }
+    
+  private static Client get(String okapiUrl, String tenantId, SAML2Configuration cfg, String samlBinding) {
+    
+    // Default lifetitme.
+    cfg.setMaximumAuthenticationLifetime(18000);
+    
+    final boolean mock = Boolean.parseBoolean(System.getProperty(HttpClientMock2.MOCK_MODE));
+    cfg.setAuthnRequestBindingType(
+      "REDIRECT".equalsIgnoreCase(samlBinding) ? 
+        SAMLConstants.SAML2_REDIRECT_BINDING_URI : 
+          SAMLConstants.SAML2_POST_BINDING_URI); // POST is the default
+    
+    Client saml2Client = mock ? new MockClient(cfg) : new Client(cfg);
+    saml2Client.setName(tenantId);
+    saml2Client.setCallbackUrl(buildCallbackUrl(okapiUrl, tenantId));
+    saml2Client.setRedirectionActionBuilder(new JsonReponseSaml2RedirectActionBuilder(saml2Client));
+    saml2Client.setStateGenerator(new SAML2StateGenerator(saml2Client));
 
+    return saml2Client;
+  }
+
+
+  private static Client get(String okapiUrl, String tenantId, String idpUrl, String keystorePassword, String actualPrivateKeyPassword, String keystoreFileName, String samlBinding) {
+    final SAML2Configuration cfg = new SAML2Configuration(
+      keystoreFileName,
+      keystorePassword,
+      actualPrivateKeyPassword,
+      idpUrl);
+
+    return get(okapiUrl, tenantId, cfg, samlBinding);
+  }
+
+
+  private static Client get(String okapiUrl, String tenantId, String keystorePassword, String privateKeyPassword, UrlResource idpUrlResource, Resource keystoreResource, String samlBinding) {
+
+    final SAML2Configuration byteArrayCfg = new SAML2Configuration(
+      keystoreResource,
+      keystorePassword,
+      privateKeyPassword,
+      idpUrlResource);
+
+    return get(okapiUrl, tenantId, byteArrayCfg, samlBinding);
+  }
 
   /**
    * Store KeyStore (as Base64 string), KeyStorePassword and PrivateKeyPassword in mod-configuration,
@@ -260,82 +333,18 @@ public class Client extends SAML2Client {
     return future.future();
 
   }
+  
+  private Client() {}
 
-
-  private static Client get(String okapiUrl, String tenantId, String idpUrl, String keystorePassword, String actualPrivateKeyPassword, String keystoreFileName, String samlBinding) {
-    final SAML2Configuration cfg = new SAML2Configuration(
-      keystoreFileName,
-      keystorePassword,
-      actualPrivateKeyPassword,
-      idpUrl);
-
-    return get(okapiUrl, tenantId, cfg, samlBinding);
-  }
-
-  private static Client get(String okapiUrl, String tenantId, String keystorePassword, String privateKeyPassword, UrlResource idpUrlResource, Resource keystoreResource, String samlBinding) {
-
-    final SAML2Configuration byteArrayCfg = new SAML2Configuration(
-      keystoreResource,
-      keystorePassword,
-      privateKeyPassword,
-      idpUrlResource);
-
-    return get(okapiUrl, tenantId, byteArrayCfg, samlBinding);
+  private Client( SAML2Configuration cfg ) {
+    super(cfg);
   }
   
-  private static Client get(String okapiUrl, String tenantId, SAML2Configuration cfg, String samlBinding) {
-    
-    // Default lifetitme.
-    cfg.setMaximumAuthenticationLifetime(18000);
-    
-    final boolean mock = Boolean.parseBoolean(System.getProperty(HttpClientMock2.MOCK_MODE));
-    cfg.setAuthnRequestBindingType(
-      "REDIRECT".equalsIgnoreCase(samlBinding) ? 
-        SAMLConstants.SAML2_REDIRECT_BINDING_URI : 
-          SAMLConstants.SAML2_POST_BINDING_URI); // POST is the default
-    
-    Client saml2Client = mock ? new MockClient(cfg) : new Client(cfg);
-    saml2Client.setName(tenantId);
-    saml2Client.setCallbackUrl(buildCallbackUrl(okapiUrl, tenantId));
-    saml2Client.setRedirectionActionBuilder(new JsonReponseSaml2RedirectActionBuilder(saml2Client));
-    saml2Client.setStateGenerator(new SAML2StateGenerator(saml2Client));
-
-    return saml2Client;
-  }
-  
-
-  private static String buildCallbackUrl(String okapiUrl, String tenantId) {
-    return okapiUrl + "/_/invoke/tenant/" + CommonHelper.urlEncode(tenantId) + Config.CALLBACK_ENDPOINT;
-  }
-  
-  private static class MockClient extends Client {
-
-    private static final Logger log = LogManager.getLogger(MockClient.class);
-    public static final String SAML_USER_ID = "saml-user-id";
-
-    public MockClient(final SAML2Configuration cfg) {
-      super(cfg);
-      log.info("SAML2 Client MOCK mode");
-    }
-
-    @Override
-    protected Optional<SAML2Credentials> retrieveCredentials(WebContext context) {
-      log.info("Mocking Client retrieveCredentials...");
-
-      assert(context.getRequestParameter("SAMLResponse").isPresent());
-
-      NameID nameId = new NameIDBuilder().buildObject();
-      String issuerId = this.getClass().getName();
-      List<Attribute> samlAttributes = new ArrayList<>();
-      Conditions conditions = new ConditionsBuilder().buildObject();
-      List<String> authnContexts = new ArrayList<>();
-      SAML2Credentials cred = new SAML2Credentials(nameId, issuerId, samlAttributes, conditions, "1", authnContexts);
-
-      CommonProfile userProfile = new CommonProfile();
-      userProfile.addAttribute("UserID", Arrays.asList(SAML_USER_ID));
-      cred.setUserProfile(userProfile);
-      return Optional.of(cred);
-    }
-
+  @Override
+  protected void initServiceProviderMetadataResolver() {
+    this.spMetadataResolver = new ExtendedSAML2ServiceProviderMetadataResolver(this.configuration,
+        computeFinalCallbackUrl(null),
+        this.credentialProvider);
+    this.spMetadataResolver.resolve();
   }
 }
---
+++ src/main/java/org/folio/sso/saml/metadata/ExtendedSAML2ServiceProviderMetadataResolver.java
@@ -0,0 +1,87 @@
+package org.folio.sso.saml.metadata;
+
+import java.util.Objects;
+
+import javax.xml.namespace.QName;
+
+import org.opensaml.saml.common.SAMLObjectBuilder;
+import org.opensaml.saml.ext.idpdisco.DiscoveryResponse;
+import org.opensaml.saml.ext.idpdisco.impl.DiscoveryResponseMarshaller;
+import org.opensaml.saml.saml2.metadata.Extensions;
+import org.opensaml.saml.saml2.metadata.IndexedEndpoint;
+import org.opensaml.saml.saml2.metadata.SPSSODescriptor;
+import org.pac4j.saml.config.SAML2Configuration;
+import org.pac4j.saml.crypto.CredentialProvider;
+import org.pac4j.saml.metadata.SAML2MetadataGenerator;
+import org.pac4j.saml.metadata.SAML2ServiceProviderMetadataResolver;
+import org.pac4j.saml.metadata.SAMLMetadataGenerator;
+
+public class ExtendedSAML2ServiceProviderMetadataResolver extends SAML2ServiceProviderMetadataResolver {
+
+  public ExtendedSAML2ServiceProviderMetadataResolver (SAML2Configuration configuration,
+      String callbackUrl, CredentialProvider credentialProvider) {
+    super(configuration, callbackUrl, credentialProvider);
+  }
+  
+  @Override
+  protected SAMLMetadataGenerator buildMetadataGenerator() {
+    final ExtendedSAML2MetadataGenerator metadataGenerator = new ExtendedSAML2MetadataGenerator();
+    super.fillSAML2MetadataGenerator(metadataGenerator);
+    addDiscoInfo(metadataGenerator);
+    return metadataGenerator;
+  }
+  
+  private void addDiscoInfo (ExtendedSAML2MetadataGenerator metadataGenerator) {
+    // ADd the discovery service.
+    metadataGenerator.setDiscoUrl("https://local.disco.service");
+  }
+
+  private static class ExtendedSAML2MetadataGenerator extends SAML2MetadataGenerator {
+
+    private String discoUrl = null;
+
+    public void setDiscoUrl (String discoUrl) {
+      this.discoUrl = discoUrl;
+    }
+
+    @Override
+    protected SPSSODescriptor buildSPSSODescriptor() {
+      final SPSSODescriptor ed = super.buildSPSSODescriptor();
+      
+      // Add the disco response if we have a disco URL set.
+      if (discoUrl != null) {
+        
+        // Grab the current extensions.
+        final Extensions ext = ed.getExtensions();
+        ext.getNamespaceManager().registerAttributeName(DiscoveryResponse.DEFAULT_ELEMENT_NAME);
+        
+        // Grab a builder for our element.
+        @SuppressWarnings("unchecked")
+        final SAMLObjectBuilder<IndexedEndpoint> discoBuilder = (SAMLObjectBuilder<IndexedEndpoint>)
+          this.builderFactory.getBuilder(DiscoveryResponse.DEFAULT_ELEMENT_NAME);
+        
+        // Build and add the element to the metadata.
+        
+        // Bug in SAML mod uses default metadata path instead of disco
+        final IndexedEndpoint discoResp = Objects.requireNonNull(discoBuilder).buildObject(
+            DiscoveryResponse.DEFAULT_ELEMENT_NAME.getNamespaceURI(),
+            DiscoveryResponse.DEFAULT_ELEMENT_NAME.getLocalPart(),
+            DiscoveryResponse.DEFAULT_ELEMENT_NAME.getPrefix()
+        );
+        discoResp.setLocation(discoUrl);
+        discoResp.setBinding(DiscoveryResponse.DEFAULT_ELEMENT_NAME.getNamespaceURI());
+        
+        // Ensure we have the marshaller registered.
+//        QName qname = discoResp.getElementQName();
+//        if (qname != null && (this.marshallerFactory.getMarshaller(qname) == null)) {
+//          this.marshallerFactory.registerMarshaller(
+//              qname, new DiscoveryResponseMarshaller() );
+//        }
+        
+        ext.getUnknownXMLObjects().add(discoResp);
+      }
+      
+      return ed;
+    }
+  }
+}
