ce1de2767b7f1b6b16254814e4170e77856d7133
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2021-09-15 13:25:32 +0100

    Move the mock to it's own file so it can be reused.

---
+++ src/test/java/org/folio/test/mock/MockHttpResponse.java
@@ -0,0 +1,95 @@
+package org.folio.test.mock;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import io.vertx.codegen.annotations.Fluent;
+import io.vertx.core.MultiMap;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpVersion;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.web.client.HttpResponse;
+
+public class MockHttpResponse<T> implements HttpResponse<T> {
+  
+  private int statusCode;
+  
+  @Fluent
+  public HttpResponse<T> setStatusCode(final int statusCode) {
+    this.statusCode = statusCode;
+    return this;
+  }
+
+  @Override
+  public HttpVersion version () {
+    return HttpVersion.HTTP_1_1;
+  }
+
+  @Override
+  public int statusCode () {
+    return statusCode;
+  }
+
+  @Override
+  public String statusMessage () {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  MultiMap headers;
+  
+  @Override
+  public MultiMap headers () {
+    if (headers == null) {
+      headers = MultiMap.caseInsensitiveMultiMap();
+    }
+    return headers;
+  }
+
+  @Override
+  public @Nullable String getHeader (String headerName) {
+    return headers().get(headerName);
+  }
+
+  MultiMap trailers;
+  @Override
+  public MultiMap trailers () {
+    if (trailers == null) {
+      trailers = MultiMap.caseInsensitiveMultiMap();
+    }
+    return trailers;
+  }
+
+  @Override
+  public @Nullable String getTrailer (String trailerName) {
+    return headers().get(trailerName);
+  }
+
+  @Override
+  public List<String> cookies () {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public @Nullable T body () {
+    return null;
+  }
+
+  @Override
+  public @Nullable Buffer bodyAsBuffer () {
+    return null;
+  }
+
+  @Override
+  public List<String> followedRedirects () {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public @Nullable JsonArray bodyAsJsonArray () {
+    return null;
+  }
+}
+
--- src/test/java/org/folio/util/HttpUtilsTest.java
+++ src/test/java/org/folio/util/HttpUtilsTest.java
@@ -3,18 +3,10 @@ package org.folio.util;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.folio.test.mock.MockHttpResponse;
 import org.junit.Test;
-
-import io.vertx.codegen.annotations.Nullable;
-import io.vertx.core.MultiMap;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.http.HttpVersion;
-import io.vertx.core.json.JsonArray;
-import io.vertx.ext.web.client.HttpResponse;
 
 public class HttpUtilsTest {
 
@@ -47,7 +39,7 @@ public class HttpUtilsTest {
   @Test
   public void twoHundredResponses() {
     for (int i=lowerBound; i<=upperLimit; i++) {
-      statusCode = i;
+      MOCK_RESPONSE.setStatusCode( i );
       assertTrue( HttpUtils.isSuccess(MOCK_RESPONSE) );
     }
   }
@@ -55,93 +47,21 @@ public class HttpUtilsTest {
   @Test
   public void noneTwoHundredResponses() {
     // Test boundraries.
-    statusCode = lowerBound - 1;
+    MOCK_RESPONSE.setStatusCode( lowerBound - 1 );
     assertFalse(HttpUtils.isSuccess(MOCK_RESPONSE));
-    statusCode = upperLimit + 1;
+    MOCK_RESPONSE.setStatusCode( upperLimit + 1 );
     assertFalse(HttpUtils.isSuccess(MOCK_RESPONSE));
     
     // Test 10 random codes for each end,excluding the limits we already tested.
     for (int i=0; i<10; i++) {
-      statusCode = ThreadLocalRandom.current().nextInt(0, lowerBound - 1);
+      MOCK_RESPONSE.setStatusCode( ThreadLocalRandom.current().nextInt(0, lowerBound - 1) );
       assertFalse(HttpUtils.isSuccess(MOCK_RESPONSE));
     }
     for (int i=0; i<10; i++) {
-      statusCode = ThreadLocalRandom.current().nextInt(upperLimit + 2, 1000);
+      MOCK_RESPONSE.setStatusCode( ThreadLocalRandom.current().nextInt(upperLimit + 2, 1000) );
       assertFalse(HttpUtils.isSuccess(MOCK_RESPONSE));
     }
   }
-
-  private static int statusCode = -1;
   
-  public static final HttpResponse<String> MOCK_RESPONSE = new HttpResponse<String>() {
-    
-    @Override
-    public HttpVersion version () {
-      return HttpVersion.HTTP_1_1;
-    }
-
-    @Override
-    public int statusCode () {
-      return statusCode;
-    }
-
-    @Override
-    public String statusMessage () {
-      // TODO Auto-generated method stub
-      return null;
-    }
-
-    MultiMap headers;
-    @Override
-    public MultiMap headers () {
-      if (headers == null) {
-        headers = MultiMap.caseInsensitiveMultiMap();
-      }
-      return headers;
-    }
-
-    @Override
-    public @Nullable String getHeader (String headerName) {
-      return headers().get(headerName);
-    }
-    
-    MultiMap trailers;
-    @Override
-    public MultiMap trailers () {
-      if (trailers == null) {
-        trailers = MultiMap.caseInsensitiveMultiMap();
-      }
-      return trailers;
-    }
-
-    @Override
-    public @Nullable String getTrailer (String trailerName) {
-      return headers().get(trailerName);
-    }
-
-    @Override
-    public List<String> cookies () {
-      return Collections.emptyList();
-    }
-
-    @Override
-    public @Nullable String body () {
-      return null;
-    }
-
-    @Override
-    public @Nullable Buffer bodyAsBuffer () {
-      return null;
-    }
-
-    @Override
-    public List<String> followedRedirects () {
-      return Collections.emptyList();
-    }
-
-    @Override
-    public @Nullable JsonArray bodyAsJsonArray () {
-      return null;
-    }
-  };
+  private MockHttpResponse<String> MOCK_RESPONSE = new MockHttpResponse<>();
 }
